e7cef395f0143d2517adfea8be416aa4ac179fa5
Aaron Tillekeratne <Aaron Tillekeratne>
2016-10-04 21:28:22 +1100

    Added regular expressions for table creation and removed header row from being a observation.

--- Hive Queries.sql
+++ Hive Queries.sql
@@ -52,6 +52,54 @@ CREATE TABLE p_Master (coachID STRING, firstName STRING, lastName STRING, birthD
 CREATE TABLE p_AwardsCoaches (coachID STRING, year INT);
 CREATE TABLE p_Coaches (coachID STRING, year INT, games INT, wins INT, losses INT, ties INT);
 
+-- Write data into persistent tables using regex.
+INSERT OVERWRITE TABLE p_Master
+SELECT
+regexp_extract(col_value, '^(?:([^,]*)\,?){2}',1) coachID,
+regexp_extract(col_value, '^(?:([^,]*)\,?){4}',1) firstName,
+regexp_extract(col_value, '^(?:([^,]*)\,?){5}',1) lastName,
+regexp_extract(col_value, '^(?:([^,]*)\,?){22}',1) birthDay,
+regexp_extract(col_value, '^(?:([^,]*)\,?){21}',1) birthMon,
+regexp_extract(col_value, '^(?:([^,]*)\,?){20}',1) birthYear,
+regexp_extract(col_value, '^(?:([^,]*)\,?){23}',1) birthCountry
+FROM temp_Master;
+
+INSERT OVERWRITE TABLE p_AwardsCoaches 
+SELECT
+regexp_extract(col_value, '^(?:([^,]*)\,?){1}',1) coachID,
+regexp_extract(col_value, '^(?:([^,]*)\,?){3}',1) year
+FROM temp_AwardsCoaches;
+
+
+
+
+INSERT OVERWRITE TABLE p_Coaches
+SELECT
+regexp_extract(col_value, '^(?:([^,]*)\,?){1}',1) coachID,
+regexp_extract(col_value, '^(?:([^,]*)\,?){2}',1) year,
+regexp_extract(col_value, '^(?:([^,]*)\,?){7}',1) games,
+regexp_extract(col_value, '^(?:([^,]*)\,?){8}',1) wins,
+regexp_extract(col_value, '^(?:([^,]*)\,?){9}',1) losses,
+regexp_extract(col_value, '^(?:([^,]*)\,?){10}',1) ties
+FROM temp_Coaches;
+
+-- Remove header from becoming data row.
+INSERT OVERWRITE TABLE p_Master
+SELECT * 
+FROM p_Master
+WHERE coachID != 'coachID';
+
+INSERT OVERWRITE TABLE p_AwardsCoaches
+SELECT * 
+FROM p_AwardsCoaches
+WHERE coachID != 'coachID';
+
+INSERT OVERWRITE TABLE p_Coaches
+SELECT * 
+FROM p_Coaches
+WHERE coachID != 'coachID';
+
+
 -- i : Coaches who have won more than 1 award
 -- FirstName, LastName, DateOfBirth, BirthCountry, NumberOfAwards
 
